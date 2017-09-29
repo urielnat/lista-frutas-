788cf9a68a6fa82f5432a6de44b18c5897de3f43
uriel <nat_7263@hotmail.com>
2017-09-29 10:48:40 -0500

    lo puse en plural

--- App2/App2/MainPage2.xaml.cs
+++ App2/App2/MainPage2.xaml.cs
@@ -22,7 +22,7 @@ namespace App2
             list.ItemsSource = new TodoItem[] {
                 new TodoItem { Name = "Comprar pera",Imagen="https://i.ytimg.com/vi/3fHynPXZAT8/maxresdefault.jpg" },
                 new TodoItem { Name = "Comprar naranjas", Done=true,Imagen="http://pngimg.com/uploads/orange/orange_PNG800.png"} ,
-                new TodoItem { Name = "Comprar mango",Imagen ="http://www.danper.com/media_danper/uploads/producto/frutas/mango-peruano-sabor-tropical.jpg" },
+                new TodoItem { Name = "Comprar mangos",Imagen ="http://www.danper.com/media_danper/uploads/producto/frutas/mango-peruano-sabor-tropical.jpg" },
                 new TodoItem { Name = "Comprar manzana", Done=true,Imagen="http://static.commentcamarche.net/sante-medecine.commentcamarche.net/pictures/9dA3Z9lr-photo-pomme-2.png" },
                 new TodoItem { Name = "Comprar platano", Done=true,Imagen="https://www.importancia.org/wp-content/uploads/platano.jpg" } //para ios solo png si es recurso
                 //si se usa un recurso es necesario ponerlo en las carpetas respectivas de cada dispositivo, si se usa una url no hay problema
